ffc3e51ea09dd2b88780f1f5d4b264bc945645da
Sebastian Bel <r0931773@student.thomasmore.be>
2023-04-25 10:52:05 +0200

    Add database file

---
+++ db/Library.sql
@@ -0,0 +1,42 @@
+create schema library;
+use library;
+
+create table Timeslot(
+timeslot_id int auto_increment not null primary key,
+start_time timestamp not null,
+end_time timestamp not null,
+date date,
+number_of_seats int
+);
+
+create table Schedule_Exception(
+schedule_exception_id int auto_increment not null primary key,
+date date not null,
+opening_time timestamp not null,
+closing_time timestamp not null
+);
+
+create table Schedule(
+schedule_id int auto_increment not null primary key,
+week_day varchar(10) not null,
+opening_time timestamp not null,
+closing_time timestamp not null
+);
+
+create table Reservation(
+reservation_id int auto_increment not null primary key,
+name varchar(20),
+first_name varchar(30),
+email varchar(180),
+timestamp timestamp,
+cancel_hash char(64)
+);
+
+
+create table Reservation_Timeslot(
+reservation_timeslot_id int auto_increment not null primary key,
+reservation_id int not null,
+timeslot_id int not null,
+constraint FK_Timeslot foreign key (timeslot_id) references Timeslot(timeslot_id),
+constraint FK_Reservation foreign key (reservation_id) references Reservation(reservation_id)
+);
