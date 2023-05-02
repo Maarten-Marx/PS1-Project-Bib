9f253dbf0e49d20e2dabf9dc4a5a21e6fe8ceab8
Sebastian Bel <r0931773@student.thomasmore.be>
2023-05-02 12:38:51 +0200

    database tweaks

--- db/Library.sql
+++ db/Library.sql
@@ -1,44 +1,49 @@
 create schema library;
 use library;
 
-create table Timeslot(
-timeslot_id int auto_increment not null primary key,
-start_time timestamp not null,
-end_time timestamp not null,
-date date,
-number_of_seats int
+create table Timeslot
+(
+    timeslot_id int auto_increment not null primary key,
+    start_time timestamp not null,
+    end_time timestamp not null,
+    date date not null,
+    number_of_seats int not null
 );
 
-create table Schedule_Exception(
-schedule_exception_id int auto_increment not null primary key,
-date date not null,
-number_of_seats int not null,
-opening_time timestamp not null,
-closing_time timestamp not null
+create table Schedule_Exception
+(
+    schedule_exception_id int auto_increment not null primary key,
+    date date not null,
+    number_of_seats int not null,
+    opening_time time,
+    closing_time time
 );
 
-create table Schedule(
-schedule_id int auto_increment not null primary key,
-week_day_index int not null,
-number_of_seats int not null,
-opening_time timestamp not null,
-closing_time timestamp not null
+create table Schedule
+(
+    schedule_id int auto_increment not null primary key,
+    week_day_index int not null,
+    number_of_seats int not null,
+    opening_time time,
+    closing_time time
 );
 
-create table Reservation(
-reservation_id int auto_increment not null primary key,
-name varchar(20),
-first_name varchar(30),
-email varchar(180),
-timestamp timestamp,
-cancel_hash char(64)
+create table Reservation
+(
+    reservation_id int auto_increment not null primary key,
+    name varchar(20) not null,
+    first_name varchar(30) not null,
+    email varchar(180) not null,
+    timestamp timestamp not null,
+    cancel_hash char(64) not null
 );
 
-
-create table Reservation_Timeslot(
-reservation_timeslot_id int auto_increment not null primary key,
-reservation_id int not null,
-timeslot_id int not null,
-constraint FK_Timeslot foreign key (timeslot_id) references Timeslot(timeslot_id),
-constraint FK_Reservation foreign key (reservation_id) references Reservation(reservation_id)
+create table Reservation_Timeslot
+(
+    reservation_timeslot_id int auto_increment not null primary key,
+    reservation_id int not null,
+    timeslot_id int not null,
+    constraint unq_reservation_timeslot unique (reservation_id, timeslot_id),
+    constraint FK_Timeslot foreign key (timeslot_id) references Timeslot (timeslot_id),
+    constraint FK_Reservation foreign key (reservation_id) references Reservation (reservation_id) on delete cascade
 );
