df7e7520d7f9925d85b7d308427edf2ad4df90fa
VanDenHeuvelRune <r0930142@student.thomasmore.be>
2023-05-13 00:21:34 +0200

    Created components for dividers (two different types)

---
+++ web/src/components/HorizontalDivider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function PrimaryHorizontalDivider() {
+    return <div className='primaryHorizontalDivider'></div>
+}
+
+export function SecondaryHorizontalDivider() {
+    return <div className='secondaryHorizontalDivider'></div>
+}
