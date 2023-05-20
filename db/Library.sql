65ef1990c8eb67dd8b6ad8ab3d1d679ebc2f58a1
VanDenHeuvelRune <r0930142@student.thomasmore.be>
2023-05-20 13:46:16 +0200

    Inserted test data

--- db/Library.sql
+++ db/Library.sql
@@ -1,7 +1,9 @@
+-- CREATE AND ACTIVATE THE DATABASE
 DROP SCHEMA IF EXISTS library;
 CREATE SCHEMA library;
 USE library;
 
+-- CREATE THE TABLES
 CREATE TABLE Timeslot (
     timeslot_id INT AUTO_INCREMENT PRIMARY KEY,
     start_time TIMESTAMP NOT NULL,
@@ -26,8 +28,7 @@ CREATE TABLE Schedule (
     closing_time TIME
 );
 
-CREATE TABLE Reservation
-(
+CREATE TABLE Reservation (
     reservation_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     first_name VARCHAR(30) NOT NULL,
@@ -36,8 +37,7 @@ CREATE TABLE Reservation
     cancel_hash CHAR(64) NOT NULL
 );
 
-CREATE TABLE Reservation_Timeslot
-(
+CREATE TABLE Reservation_Timeslot (
     reservation_timeslot_id INT AUTO_INCREMENT PRIMARY KEY,
     reservation_id INT NOT NULL,
     timeslot_id INT NOT NULL,
@@ -45,3 +45,12 @@ CREATE TABLE Reservation_Timeslot
     CONSTRAINT FK_Timeslot FOREIGN KEY (timeslot_id) REFERENCES Timeslot (timeslot_id),
     CONSTRAINT FK_Reservation FOREIGN KEY (reservation_id) REFERENCES Reservation (reservation_id) ON DELETE CASCADE
 );
+
+-- INSERT TEST DATA
+INSERT INTO library.schedule (schedule_id, week_day_index, number_of_seats, opening_time, closing_time) VALUES (7, 0, 20, '10:00:00', '19:30:00'),
+                                                                                                               (8, 1, 20, '10:00:00', '19:30:00'),
+                                                                                                               (9, 2, 20, '13:00:00', '17:00:00'),
+                                                                                                               (10, 3, 20, '13:00:00', '17:00:00'),
+                                                                                                               (11, 4, 20, '13:00:00', '17:00:00'),
+                                                                                                               (12, 5, 10, '10:00:00', '14:00:00'),
+                                                                                                               (13, 6, 0, null, null);
